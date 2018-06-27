319df8e44ef4a747807923876df597bea2e1fac0
Nicolas Marini <nmarini44@gmail.com>
2018-06-27 00:05:11 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist 
+attr_accessor :name, :all_songs 
+@@all = []
+  def initialize(name)
+    @name = name
+    @all_songs = []
+    @@all << self    
+  end
+
+  def self.all 
+    @@all 
+  end
+
+
+end
